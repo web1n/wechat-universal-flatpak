1ad002fb166c19bb3dddfb9cd59d01dbdd00365c
Bin <9633157+web1n@users.noreply.github.com>
2024-04-03 15:50:33 +0800

    Update redirect.c

--- redirect.c
+++ redirect.c
@@ -1,6 +1,7 @@
 #define _GNU_SOURCE
 #include <dlfcn.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/types.h>
 
 
@@ -8,6 +9,27 @@ int* (*real_access)(const char*, int);
 void* (*real_dlopen)(char const*, int);
 FILE* (*real_fopen)(char const*, char const*);
 FILE* (*real_fopen64)(char const*, char const*);
+
+void hook_path(const char* func, const char** path) {
+    char* redirect = NULL;
+
+    if (strcmp(*path, "/usr/lib/license/libuosdevicea.so") == 0) {
+        redirect = "/app/wechat/libuosdevicea.so";
+    } else if (strcmp(*path, "/etc/os-release") == 0) {
+        redirect = "/app/license/etc/os-release";
+    } else if (strcmp(*path, "/etc/lsb-release") == 0) {
+        redirect = "/app/license/etc/lsb-release";
+    } else if (strcmp(*path, "/var/lib/uos-license/.license.json") == 0) {
+        redirect = "/app/license/var/lib/uos-license/.license.json";
+    } else if (strcmp(*path, "/var/uos/.license.key") == 0) {
+        redirect = "/app/license/var/uos/.license.key";
+    }
+
+    if (redirect != NULL) {
+        printf("%s: redirect from %s to: %s\n", func, *path, redirect);
+        *path = redirect;
+    }
+}
 
 int* access(const char* __file, int __mode) {
     if (real_access == NULL) {
@@ -55,25 +77,4 @@ FILE* fopen64(const char* __file, const char* __mode) {
     }
 
     return (*real_fopen64)(__file, __mode);
-}
-
-void hook_path(const char* func, const char** path) {
-    char* redirect = NULL;
-
-    if (strcmp(*path, "/usr/lib/license/libuosdevicea.so") == 0) {
-        redirect = "/app/wechat/libuosdevicea.so";
-    } else if (strcmp(*path, "/etc/os-release") == 0) {
-        redirect = "/app/license/etc/os-release";
-    } else if (strcmp(*path, "/etc/lsb-release") == 0) {
-        redirect = "/app/license/etc/lsb-release";
-    } else if (strcmp(*path, "/var/lib/uos-license/.license.json") == 0) {
-        redirect = "/app/license/var/lib/uos-license/.license.json";
-    } else if (strcmp(*path, "/var/uos/.license.key") == 0) {
-        redirect = "/app/license/var/uos/.license.key";
-    }
-
-    if (redirect != NULL) {
-        printf("%s: redirect from %s to: %s\n", func, *path, redirect);
-        *path = redirect;
-    }
 }
